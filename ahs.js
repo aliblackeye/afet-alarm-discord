15af0ffe7bf35ae891ff44a8e4c9cd675b7c2887
Berkay ÇAĞLAR <berkay_caglar123@hotmail.com>
2023-02-09 19:52:35 +0300

    Updated+

--- ahs.js
+++ ahs.js
@@ -1,10 +1,22 @@
+const {
+
+  DMChannel,
+} = require("discord.js");
+
+// Import Utils
+const createEmbed = require("./utils/createEmbed.js");
+
+const embed = createEmbed(symbol, change.toFixed(2), symbolsPrices[symbol].oldPrice, newPrice);
+
 const berkay_devChannel = new DMChannel(client, {
   id: "1073265716221972561",
   name: "berkay-dev",
 });
 
-function ahs() {
+async function ahs() {
   console.log("AHS Bot")
+  //await berkay_devChannel.send({ embeds: [embed], });
+  await berkay_devChannel.send({ content: 'This is an embed.', });
 }
 
 module.exports = {
